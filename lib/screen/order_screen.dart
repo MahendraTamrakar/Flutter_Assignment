2cbcfe84de855c16ef5dd213b2583626036c3343
MahendraTamrakar <tamrakarmahendra157@gmail.com>
2025-09-28 01:47:12 +0530

    minor Changes

--- lib/screen/order_screen.dart
+++ lib/screen/order_screen.dart
@@ -18,7 +18,7 @@ class _OrdersScreenState extends State<OrdersScreen> with SingleTickerProviderSt
     
     // Set status bar style
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
+      statusBarColor: const Color.fromARGB(0, 255, 255, 255),
       statusBarIconBrightness: Brightness.dark,
       statusBarBrightness: Brightness.light,
     ));
